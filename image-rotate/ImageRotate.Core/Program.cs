540414028e68bd7857a2dd746624a4dbb5815963
dnquangit <dnquangit@gmail.com>
2025-11-08 13:20:17 +0700

    Add image rotate project

---
+++ image-rotate/ImageRotate.Core/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace ImageRotate.Core;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        string path = @"C:\temp\image-rotate\image-rotate\teapot.bmp";
+        byte[] bytes = File.ReadAllBytes(path);
+
+        //// Chuyển mảng byte → chuỗi hex kiểu “AA BB CC DD …”
+        
+
+        //HexViewer.HexDump(path);
+    }
+
+    private string ToHex(byte input)
+    {
+        return input.ToString("X2");
+    }
+}
+
